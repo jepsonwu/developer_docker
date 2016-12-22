499da319aaadab96ef0a5801f00561484954831d
shanzha <shanzha@in66.com>
2016-12-22 15:21:58 +0800

    optimize php7

--- php7/build.sh
+++ php7/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker stop php%suffix%
-docker rm php%suffix%
-docker rmi %auth%/php%suffix%
+docker stop php7%suffix%
+docker rm php7%suffix%
+docker rmi %auth%/php7%suffix%
 
-docker build -t %auth%/php%suffix% .
+docker build -t %auth%/php7%suffix% .
